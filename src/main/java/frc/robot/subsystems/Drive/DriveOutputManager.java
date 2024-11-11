cc941c5a92bb9c0e4c2d9a7e04c5a8534a16c7cd
PotmanNob <Potmannob@outlook.com>
2024-11-10 21:48:02 -0600

    abc

--- src/main/java/frc/robot/subsystems/Drive/DriveOutputManager.java
+++ src/main/java/frc/robot/subsystems/Drive/DriveOutputManager.java
@@ -7,16 +7,18 @@ import com.ctre.phoenix6.swerve.SwerveDrivetrain.SwerveDriveState;
 
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.math.kinematics.SwerveModuleState;
+import edu.wpi.first.math.util.Units;
 
 public class DriveOutputManager {
+    static final String sussystemname = "Drive";
     static Pose2d lastPose = new Pose2d();
     static double lastTime = 0;
 
     public static void processSwerveDriveState(SwerveDriveState swerveDriveState) {
         Pose2d currentPose2d = swerveDriveState.Pose;
         double currentTime = Utils.getSystemTimeSeconds();
-        double timeDiff = currentTime - lastTime;
         Translation2d distanceDiff = currentPose2d.minus(lastPose).getTranslation();
         lastPose = currentPose2d;
         lastTime = currentTime;
@@ -25,7 +27,8 @@ public class DriveOutputManager {
         Logger.recordOutput("Drive/RealOutput/robotPose", currentPose2d);
         Logger.recordOutput("Drive/RealOutput/currentTIme", currentTime);
         Logger.recordOutput("Drive/RealOutput/distanceDiff", distanceDiff);
-        Logger.recordOutput("Drive/RealOutput/velocity", distanceDiff.div(timeDiff));
+        Logger.recordOutput("Drive/RealOutput/chassisSpeed", swerveDriveState.Speeds);
+        Logger.recordOutput("Drive/RealOutput/velocity", Units.metersToFeet(Math.hypot(swerveDriveState.Speeds.vxMetersPerSecond, swerveDriveState.Speeds.vyMetersPerSecond)));
         Logger.recordOutput("Drive/RealOutput/swerveModuleStates", swerveDriveState.ModuleStates);
         Logger.recordOutput("Drive/RealOutput/swerveModulePosition", swerveDriveState.ModulePositions);
 
