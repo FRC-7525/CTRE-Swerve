bcf5e2e926951ce5edfb957578517961264c02c5
PotmanNob <Potmannob@outlook.com>
2024-12-14 17:11:59 -0600

    logs to usb

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -7,6 +7,7 @@ package frc.robot;
 import org.littletonrobotics.junction.LoggedRobot;
 import org.littletonrobotics.junction.Logger;
 import org.littletonrobotics.junction.networktables.NT4Publisher;
+import org.littletonrobotics.junction.wpilog.WPILOGWriter;
 
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
@@ -21,7 +22,21 @@ public class Robot extends LoggedRobot {
 
     @Override
     public void robotInit() {
-        Logger.addDataReceiver(new NT4Publisher());
+
+        switch (Constants.ROBOT_MODE) {
+            case REAL:
+                Logger.addDataReceiver(new NT4Publisher());
+                Logger.addDataReceiver(new WPILOGWriter());
+                break;
+        
+            case SIM:
+                Logger.addDataReceiver(new NT4Publisher());
+                break;
+
+            case TESTING:
+                break;
+        }
+
 		Logger.start();
         CommandsUtil.logCommands();
 		DriverStation.silenceJoystickConnectionWarning(true);
